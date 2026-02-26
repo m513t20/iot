cc5fe05c24afab96726257c83473b38e510497da
sototh <sad@daw>
2026-02-26 12:40:04 +0800

    esp base

---
+++ esp_network/esp_network.ino
@@ -0,0 +1,12 @@
+#include "Config.h"
+#include "WIFI.h"
+
+void setup() {
+  // put your setup code here, to run once:
+    init_WIFI(WIFI_START_MODE_CLIENT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

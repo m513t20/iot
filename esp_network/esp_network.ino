c61cd5c2726d6a0bc9c118b1d4f558ec9c046ac9
sototh <sad@daw>
2026-02-26 12:54:37 +0800

    added mac id

--- esp_network/esp_network.ino
+++ esp_network/esp_network.ino
@@ -3,10 +3,14 @@
 
 void setup() {
   // put your setup code here, to run once:
-    init_WIFI(WIFI_START_MODE_CLIENT);
+  Serial.begin(115200);
+  init_WIFI(WIFI_START_MODE_CLIENT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  delay(500);
+  Serial.print("our id is: ");
+  Serial.println(id());
 }
